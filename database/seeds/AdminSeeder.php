4d864464360e8f4180facbf50d0862791e177614
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2018-08-17 17:59:01 +0400

    user migration and admin seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+            'remember_token' => str_random(60),
+        ]);
+    }
+}
