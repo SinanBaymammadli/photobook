71e6373f99127c2c3f513b69c1e3d28348afbd03
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2019-03-23 11:15:40 +0400

    photo

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -18,7 +18,7 @@ class OrderController extends Controller
      */
     public function __construct()
     {
-        // $this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     /**
